ea96c4be89bd9f3c86953a81141dcd72aeb5acca
rushikeshkadam449 <107835650+rushikeshkadam449@users.noreply.github.com>
2026-08-26 21:24:29 +0530

    Time: 4 ms (44.23%) | Memory: 52.1 MB (54.89%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,26 +1,35 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+      //  Map<Integer, Integer> map = new HashMap<>();
         int left = 0;
         int res = 0;
+        int zeroes = 0;
 
-        for(int right = 0; right < nums.length; right++) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+        for (int right = 0; right < nums.length; right++) {
 
+            if(nums[right] == 0){
+                zeroes++;
+            }
             int currentWindow = right - left + 1;
 
-            while(currentWindow - map.getOrDefault(1, 0) > k) {
-                map.put(nums[left], map.getOrDefault(nums[left], 0) - 1);
-
-               // if(map.get())
-               left++;
-               currentWindow = right - left + 1;
-
-
-
+            while(zeroes > k){
+                if(nums[left] == 0){
+                    zeroes--;
+                }
+                left++;
             }
-            
-            res = Math.max(res, right - left + 1);
+            res = Math.max(res, right - left +1);
+           // map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+            // int currentWindow = right - left + 1;
+
+            // while (currentWindow - map.getOrDefault(1, 0) > k) {
+            //     map.put(nums[left], map.getOrDefault(nums[left], 0) - 1);
+            //     left++;
+            //     currentWindow = right - left + 1;
+            // }
+
+          //  res = Math.max(res, right - left + 1);
         }
         return res;
 
